635608b275a2cd0855f6b0b684a98295a9d14d05
Reddy57 <abhilash.gaddam@antra.com>
2020-01-13 14:27:03 -0500

    updates

--- MovieShop.Entities/Common/PaginatedList.cs
+++ MovieShop.Entities/Common/PaginatedList.cs
@@ -5,39 +5,33 @@ using System.Linq.Expressions;
 
 namespace MovieShop.Entities.Common
 {
-    public class PaginatedList<T>: List<T>
+    public class PaginatedList<T> : List<T>
     {
-        public int PageIndex { get; private set; }
-        public int TotalPages { get; private set; }
-        public int TotalCount { get; private set; }
         public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
             PageIndex = pageIndex;
-            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            TotalPages = (int) Math.Ceiling(count / (double) pageSize);
             TotalCount = count;
             AddRange(items);
         }
 
-        public bool HasPreviousPage => (PageIndex > 1);
-        public bool HasNextPage => (PageIndex < TotalPages);
+        public int PageIndex { get; }
+        public int TotalPages { get; }
+        public int TotalCount { get; }
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
 
         public static PaginatedList<T> GetPaged(IQueryable<T> source, int pageIndex, int pageSize,
                                                 Func<IQueryable<T>, IOrderedQueryable<T>> orderedQuery = null,
                                                 Expression<Func<T, bool>> filter = null)
         {
+            var query = source;
+            if (filter != null) query = query.Where(filter);
 
-            IQueryable<T> query = source;
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
+            if (orderedQuery != null) query = orderedQuery(query);
 
-            if (orderedQuery != null)
-            {
-                query = orderedQuery(query);
-            }
 
-           
             var count = query.Count();
             var items = query.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
